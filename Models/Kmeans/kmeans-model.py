01c0dd3a0ff23e1ffc79919484fc213e6611e734
HJZR2004 <A01027743@tec.mx>
2025-09-18 09:40:55 -0600

    changues a little bit the kmeans, also i push the logistic regression, and finally im pushing the csv files

--- Models/Kmeans/kmeans-model.py
+++ Models/Kmeans/kmeans-model.py
@@ -17,38 +17,35 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # ==========================
-# Visualización antes de clasificar
+# PCA
 # ==========================
 pca = PCA(n_components=2)
 X_pca = pca.fit_transform(X_scaled)
 
+# Obtener los pesos de las variables en los componentes principales
+loadings = pd.DataFrame(
+    pca.components_.T,
+    columns=["PC1", "PC2"],
+    index=X.columns
+)
+print("Pesos de las características en las componentes principales:")
+print(loadings)
+
+# ==========================
+# Visualización antes de clasificar
+# ==========================
 plt.figure(figsize=(8, 6))
 plt.scatter(X_pca[:, 0], X_pca[:, 1], alpha=0.6, cmap="viridis")
 plt.title("Datos Originales (PCA 2D)")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
+plt.xlabel("Esfuerzo físico (PC1)")
+plt.ylabel("Intensidad cardíaca (PC2)")
 plt.grid(True)
-plt.show()
-
-# ==========================
-# Método del codo
-# ==========================
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, random_state=42, n_init=10)
-    kmeans.fit(X_scaled)
-    wcss.append(kmeans.inertia_)
-
-plt.plot(range(1, 11), wcss, marker="o")
-plt.xlabel("Número de clusters")
-plt.ylabel("WCSS")
-plt.title("Método del codo - KMeans")
 plt.show()
 
 # ==========================
 # Entrenar con k clusters
 # ==========================
-kmeans = KMeans(n_clusters=2, random_state=42, n_init=10)
+kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
 df["cluster"] = kmeans.fit_predict(X_scaled)
 
 # Obtener los centroides
@@ -64,8 +61,8 @@ plt.figure(figsize=(8, 6))
 plt.scatter(X_pca[:, 0], X_pca[:, 1], c=df["cluster"], cmap="viridis", alpha=0.6, label="Datos")
 plt.scatter(centroids_pca[:, 0], centroids_pca[:, 1], c="red", marker="X", s=200, label="Centroides")
 plt.title("Clusters y Centroides (PCA 2D)")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
+plt.xlabel("Esfuerzo físico (PC1)")
+plt.ylabel("Intensidad cardíaca (PC2)")
 plt.colorbar(label="Cluster")
 plt.legend()
 plt.grid(True)
